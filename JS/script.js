2dc5e8b73a5da1e53f33abbb347cedc2448dd9e9
Zaytseva Katya <katya.zaitseva05@gmail.com>
2023-03-26 20:22:35 +0300

    add 2 function

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,52 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let computerMove = 'Empty';
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'stone';
+    } else if (argMoveId == 2){
+      return 'scissors';
+    } else if (argMoveId == 3){
+      return 'paper';
+    } else return 'Empty';
+}
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/*if(randomNumber == 1){
   computerMove = 'stone';
 } else if (randomNumber == 2){
   computerMove = 'scissors';
 } else computerMove = 'paper';
-    
 
-printMessage('Computer choose: ' + computerMove);
+printMessage('Computer choose: ' + computerMove);*/
 
 let playerInput = prompt('Choose your move! 1: stone, 2: scissors, 3: paper.');
-let playerMove = 'Empty';
-
-if(playerInput == 1){
+let playerMove = getMoveName(playerInput);
+/*if(playerInput == 1){
     playerMove = 'stone';
   } else if (playerInput == 2){
     playerMove = 'scissors';
   } else if (playerInput == 3) {
     playerMove = 'paper';
-  } else printMessage('Choose error number');
+  } else printMessage('Choose error number');*/
 
-if ((playerMove == 'stone' && computerMove == 'stone') ||
-    (playerMove == 'scissors' && computerMove == 'scissors') ||
-    (playerMove == 'paper' && computerMove == 'paper'))
-{
-    printMessage('draw');
+function displayResult(argComputerMove, argPlayerMove){
+    console.log('moves:', argComputerMove, argPlayerMove);
+    printMessage('Computer choose ' + argComputerMove + ', and you ' + argPlayerMove);
+    if (playerMove == 'Empty') {
+        printMessage('You enter error number');
+        return;
+    }
+    if (playerMove == computerMove){
+        printMessage('draw');
+        return;
+    } 
+    if ((playerMove == 'stone' && computerMove == 'paper') ||
+        (playerMove == 'scissors' && computerMove == 'stone') ||
+        (playerMove == 'paper' && computerMove == 'scissors'))
+    {
+        printMessage('you lose');
+    } else printMessage('you win');
 }
-if ((playerMove == 'stone' && computerMove == 'paper') ||
-    (playerMove == 'scissors' && computerMove == 'stone') ||
-    (playerMove == 'paper' && computerMove == 'scissors'))
-{
-    printMessage('you lose');
-}
-if ((playerMove == 'stone' && computerMove == 'scissors') ||
-    (playerMove == 'scissors' && computerMove == 'paper') ||
-    (playerMove == 'paper' && computerMove == 'stone'))
-{
-    printMessage('you win');
-}
+
+displayResult(computerMove,playerMove);
+
