a2a9169903aebf12c54a76c042538c464fdbb207
Zaytseva Katya <katya.zaitseva05@gmail.com>
2023-03-26 19:12:51 +0300

    Add your choose

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,5 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-/*console.log('Wylosowana liczba to: ' + randomNumber);*/
-
 let computerMove = 'Empty';
 
 if(randomNumber == 1){
@@ -12,3 +10,14 @@ if(randomNumber == 1){
     
 
 printMessage('Computer choose: ' + computerMove);
+
+let playerInput = prompt('Choose your move! 1: stone, 2: scissors, 3: paper.');
+let playerMove = 'Empty';
+
+if(playerInput == 1){
+    playerMove = 'stone';
+  } else if (playerInput == 2){
+    playerMove = 'scissors';
+  } else playerMove = 'paper';
+
+printMessage('Twój ruch to: ' + playerMove);
